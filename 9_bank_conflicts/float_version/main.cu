3e775b6f5345953bad2b4f71a0fb0fa328609637
EC2 Default User <ec2-user@ip-172-31-23-23.ap-northeast-2.compute.internal>
2020-02-27 07:08:03 +0000

    feat: add examples of vid 9 Add examples of video 9: "NVIDIA CUDA Tutorial 9: Bank Conflicts."

---
+++ 9_bank_conflicts/float_version/main.cu
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <ctime>
+#include <cuda.h>
+
+__global__
+void MyKernel (unsigned long long *time)
+{
+	__shared__ float shared[1024];
+	unsigned long long startTime = std::clock();
+
+	shared[threadIdx.x*32]++;
+
+	unsigned long long finishTime = std::clock();
+
+	*time = (finishTime - startTime);
+}
+
+
+int main ()
+{
+	unsigned long long time;
+	unsigned long long *d_time;
+
+	cudaMalloc(&d_time, sizeof(unsigned long long));
+
+	for (int i = 0; i < 10; ++i) {
+		MyKernel<<<1, 32>>>(d_time);
+
+		cudaMemcpy(&time, d_time, sizeof(unsigned long long), cudaMemcpyDeviceToHost);
+
+		// 14 is the overhead for calling clock
+		std::cout << "Time: " << (time - 14) / 32 << std::endl;
+
+		std::cout << std::endl;
+	}
+
+	cudaFree(d_time);
+
+	std::cin.get();
+
+	cudaDeviceReset();
+
+	return 0;
+}
+
